a7aebb5c3b4adf74d020da3a1087ea42b78ee073
codejunky <oussama.bouguerne@gmail.com>
2024-09-01 02:40:54 +0300

    Implement timer functionality

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,54 @@
+let tasks = [];
+
+function updateTime() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = document.getElementById("time");
+    const minutes = `${25 - res.timer / 60}`.padStart(2, "0");
+    const seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    time.textContent =
+      res.timer > 0 ? `${Math.trunc(minutes)}:${seconds}` : "25:00";
+  });
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
 
-let tasks = [];
+const startTimerBtn = document.getElementById("start-timer-btn");
+
+chrome.storage.local.get(["isRunning"], (res) => {
+  startTimerBtn.textContent = res.isRunning ? "Pause Timer" : "Start Timer";
+});
+
+startTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.get(["isRunning"], (res) => {
+    chrome.storage.local.set(
+      {
+        isRunning: !res.isRunning,
+      },
+      () => {
+        startTimerBtn.textContent = !res.isRunning
+          ? "Pause Timer"
+          : "Start Timer";
+      }
+    );
+  });
+});
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+  chrome.storage.local.set(
+    {
+      timer: 0,
+      isRunning: false,
+    },
+    () => {
+      startTimerBtn.textContent = "Start Timer";
+    }
+  );
+});
 
 chrome.storage.sync.get(["tasks"], (res) => {
   tasks = res.tasks ?? [];
